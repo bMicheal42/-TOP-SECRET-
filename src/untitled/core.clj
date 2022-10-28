a2277abcbd162f53452952fde3f1bc38226f12df
bMicheal42 <g.taftin@21-school.ru>
2022-10-28 13:29:23 +0400

    test deploy

--- src/untitled/core.clj
+++ src/untitled/core.clj
@@ -122,12 +122,12 @@
 
 (defn handler [request]
   {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body "<h1>I WILL BECOME A SAMURAI</h1>"})
+   :headers {"Content-Type" "text/html"}❤️
+   :body "<h1>Глеб <3</h1>"})
 
 (defn -main [& args]
   (ring/run-jetty handler {:port 80 :join? false}))
-
+❤️
 ;(comment
 ;  (list-patients)
 ;
